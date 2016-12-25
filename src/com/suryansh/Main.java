2cc89938d57656cd6b03972e66ff12c8f61a6d8d
Suryansh <Frixoe@users.noreply.github.com>
2016-12-25 16:56:56 +0900

    Update Main.java

--- src/com/suryansh/Main.java
+++ src/com/suryansh/Main.java
@@ -1,4 +1,19 @@
 package com.suryansh;
 
+import com.suryansh.model.Game;
+import com.suryansh.model.Output;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        Output output = new Output(game);
+
+        output.declareBoards();
+
+        while (!game.isWonByPlayer() && game.getRemainingTries() > 0) {
+            output.displayBoards();
+            output.promptForAttack();
+        }
+    }
 }
