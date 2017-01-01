a8352de61dbb7f21a55d645ad6d35a1dd1e5ee8a
dewoch2 <boris.roche.971@gmail.com>
2017-01-01 21:13:38 +0100

    FormatNumberException ad display board management

--- src/com/suryansh/Main.java
+++ src/com/suryansh/Main.java
@@ -14,12 +14,19 @@ public class Main {
         Output output = new Output(game);
 
         output.declareBoards();
-        output.displayBoards();
+
+        boolean mustDisplayBoard = true;
 
         do {
 
-            System.out.println("It's your turn!");
-            System.out.printf(" You have %d tries remaining!%n%n", game.getRemainingTries());
+            if(mustDisplayBoard) {
+                output.displayBoards();
+
+                System.out.println("It's your turn!");
+                System.out.printf(" You have %d tries remaining!%n%n", game.getRemainingTries());
+            }
+
+            mustDisplayBoard = true;
             try {
                 output.playerAttacks();
                 TimeUnit.SECONDS.sleep(2);
@@ -29,6 +36,7 @@ public class Main {
             } catch (NumberFormatException nfe) {
             	
             	System.out.println("Please enter an Integer!");
+                mustDisplayBoard = false;
             	
             } catch (IllegalArgumentException e) {
                 System.out.printf("%s%n", e.getMessage());
